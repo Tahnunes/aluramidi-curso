9e5663e3256cedfb493e80c2d8ddd01433c671c9
Talita Nunes <talitanunes.tlt@hotmail.com>
2023-04-10 17:34:50 +0100

    JS

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play()
+//document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador + 1;
+
+    //console.log(contador);
 }
-
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-document.querySelector('#som_tecla_clap')
-
